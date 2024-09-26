11f34b4ab433d8dba46c25a17a3abc9e789e4387
kybex11 <artm.22@internet.ru>
2024-09-26 16:41:23 +0000

    e

---
+++ app/frontend/src/components/editor/menu.tsx
@@ -0,0 +1,16 @@
+import Editor from "./editor";
+export default function EditorMenu() {
+    return (
+        <>
+        <br /><br />
+        <div className="container-form">
+        <br />
+        <h1>Create or Open Project</h1>
+        <br />
+        <button className="button">Create</button>
+        <button className="button">Open</button>
+        </div>
+
+        </>
+    )
+}
--- app/frontend/src/components/menu/views/studio.tsx
+++ app/frontend/src/components/menu/views/studio.tsx
@@ -1,11 +1,11 @@
+import EditorMenu from "../../editor/menu"
 import Navbar from "./navbar"
 
 export default function Studio() {
     return (
         <>
         <Navbar/>
-        <br />
-        <h1>studio page</h1>
+        <EditorMenu/>
         </>
     )
 }
